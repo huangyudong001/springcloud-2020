b3d23aeb5c8656a9349eb59a717eb94b1613a55c
黄玉东 <1410995914@qq.com>
2020-09-05 14:34:54 +0800

    hystrix服务降级fallback的全局使用

--- springcloud-01/cloud-consumer-feign-hystrix-order80/src/main/java/com/hyd/springcloud/controller/OrderHystrixController.java
+++ springcloud-01/cloud-consumer-feign-hystrix-order80/src/main/java/com/hyd/springcloud/controller/OrderHystrixController.java
@@ -1,8 +1,10 @@
 package com.hyd.springcloud.controller;
 
 import com.hyd.springcloud.service.OrderHystrixServicr;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequestMapping("/order")
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")  //全局的
 public class OrderHystrixController {
 
     @Autowired
@@ -31,9 +34,10 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+    /*@HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
             @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "5000")  //3秒钟以内就是正常的业务逻辑
-    })
+    })*/ //一条一条配置fallback降级方法，不适用，会造成膨胀
+    @HystrixCommand
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
         String result = orderHystrixServicr.paymentInfo_timeOut(id);
         return result;
@@ -42,5 +46,10 @@ public class OrderHystrixController {
     //兜底方法
     public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id){
         return "我是消费者80，对付支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,(┬＿┬)";
+    }
+
+    //全局配置fallback方法
+    public String payment_Global_FallbackMethod(){
+        return "Global异常处理信息，请稍后再试,(┬＿┬)";
     }
 }
