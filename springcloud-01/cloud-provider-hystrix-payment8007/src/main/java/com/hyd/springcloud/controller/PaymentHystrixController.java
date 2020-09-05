a3635c57060515871cedbbe7cc0f31232f8a8730
黄玉东 <1410995914@qq.com>
2020-09-05 16:02:32 +0800

    hystrix服务熔断

--- springcloud-01/cloud-provider-hystrix-payment8007/src/main/java/com/hyd/springcloud/controller/PaymentHystrixController.java
+++ springcloud-01/cloud-provider-hystrix-payment8007/src/main/java/com/hyd/springcloud/controller/PaymentHystrixController.java
@@ -38,4 +38,14 @@ public class PaymentHystrixController {
         log.info("--------------result-----：" + result);
         return result;
     }
+
+    //===服务熔断
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentCircuitBreaker(id);
+        log.info("*******result:"+result);
+        return result;
+    }
+
+
 }
