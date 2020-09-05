0fdabb04b3df195955b3e9fde8ef4379b58b0e60
黄玉东 <1410995914@qq.com>
2020-09-05 14:15:48 +0800

    hystrix服务降级fallback的使用

--- springcloud-01/cloud-consumer-feign-hystrix-order80/src/main/java/com/hyd/springcloud/controller/OrderHystrixController.java
+++ springcloud-01/cloud-consumer-feign-hystrix-order80/src/main/java/com/hyd/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,8 @@
 package com.hyd.springcloud.controller;
 
 import com.hyd.springcloud.service.OrderHystrixServicr;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,8 +30,17 @@ public class OrderHystrixController {
         return orderHystrixServicr.paymentInfo_OK(id);
     }
 
-    @GetMapping("/hystrix/timeOut/{id}")
-    public String paymentInfo_timeOut(@PathVariable("id") Integer id){
-        return orderHystrixServicr.paymentInfo_timeOut(id);
+    @GetMapping("/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "5000")  //3秒钟以内就是正常的业务逻辑
+    })
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        String result = orderHystrixServicr.paymentInfo_timeOut(id);
+        return result;
+    }
+
+    //兜底方法
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id){
+        return "我是消费者80，对付支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,(┬＿┬)";
     }
 }
--- springcloud-01/cloud-provider-hystrix-payment8007/src/main/java/com/hyd/springcloud/service/PaymentHystrixService.java
+++ springcloud-01/cloud-provider-hystrix-payment8007/src/main/java/com/hyd/springcloud/service/PaymentHystrixService.java
@@ -1,5 +1,7 @@
 package com.hyd.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -19,14 +21,20 @@ public class PaymentHystrixService {
     }
 
     //失败
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "5000")  //3秒钟以内就是正常的业务逻辑
+    })
     public String paymentInfo_TimeOut(Integer id){
-        int timeNumber=3;
-        try {
-            TimeUnit.SECONDS.sleep(timeNumber);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return "线程池"+Thread.currentThread().getName()+"      paymentInfo_TimeOut,id：  "+id+"\t"+"失败啦，呜呜呜"+"耗时（秒）"+timeNumber;
+         int timeNumber = 3;
+//        int age = 10/0;
+         try { TimeUnit.SECONDS.sleep(timeNumber); }catch (Exception e) {e.printStackTrace();}
+        return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"呜呜呜"+" 耗时(秒)"+timeNumber;
+//        return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"呜呜呜"+" 耗时(秒)";
 
+    }
+
+    //兜底方法，降级的方法
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "线程池："+Thread.currentThread().getName()+"   系统繁忙, 请稍候再试  ,id：  "+id+"\t"+"哭了哇呜";
     }
 }
