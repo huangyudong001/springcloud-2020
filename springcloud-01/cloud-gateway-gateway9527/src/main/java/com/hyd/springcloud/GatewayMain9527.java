f49a3f250092dc55874db79bdb76a124ad587e63
黄玉东 <1410995914@qq.com>
2020-09-07 11:37:56 +0800

    Gateway网关入门yml配置

---
+++ springcloud-01/cloud-gateway-gateway9527/src/main/java/com/hyd/springcloud/GatewayMain9527.java
@@ -0,0 +1,19 @@
+package com.hyd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author 黄玉东
+ * @Date 2020/9/7 11:21
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class,args);
+    }
+}
