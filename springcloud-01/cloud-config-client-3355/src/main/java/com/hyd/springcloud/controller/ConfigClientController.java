9005a94a109dfc6d352a8ddcb4b7c732e4939f38
黄玉东 <1410995914@qq.com>
2020-09-17 16:01:42 +0800

    config客户端配置读取git文件

---
+++ springcloud-01/cloud-config-client-3355/src/main/java/com/hyd/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.hyd.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author 黄玉东
+ * @Date 2020/9/17 15:34
+ * @Version 1.0
+ */
+@RestController
+public class ConfigClientController {
+
+    // 因为config仓库以rest形式暴露，所以所有客户端都可以通过config服务端访问到github上对应的文件信息
+    @Value("${config.info}")  //这里是giehub上配置文件的信息
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return  configInfo;
+    }
+}
