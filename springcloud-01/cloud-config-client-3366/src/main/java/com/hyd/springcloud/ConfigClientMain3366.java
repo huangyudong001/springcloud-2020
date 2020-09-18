8539c31ee3ef2c60b1d340ae5f3bb88614979bf3
黄玉东 <1410995914@qq.com>
2020-09-18 09:47:24 +0800

    Config结合bus动态刷新和RabbitMQ

---
+++ springcloud-01/cloud-config-client-3366/src/main/java/com/hyd/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package com.hyd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author 黄玉东
+ * @Date 2020/9/18 9:15
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
