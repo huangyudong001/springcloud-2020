97c3b67faa075cd5ea83319791550ee2b2e46c14
黄玉东 <1410995914@qq.com>
2020-09-21 09:59:33 +0800

    stream消息驱动消费者

---
+++ springcloud-01/cloud-stream-rabbitmq-consumer8802/src/main/java/com/hyd/springcloud/StreamMQMain8802.java
@@ -0,0 +1,17 @@
+package com.hyd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 黄玉东
+ * @Date 2020/9/21 9:21
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class,args);
+    }
+}
---
+++ springcloud-01/cloud-stream-rabbitmq-consumer8803/src/main/java/com/hyd/springcloud/StreamMQMain8803.java
@@ -0,0 +1,16 @@
+package com.hyd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 黄玉东
+ * @Date 2020/9/21 9:39
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
