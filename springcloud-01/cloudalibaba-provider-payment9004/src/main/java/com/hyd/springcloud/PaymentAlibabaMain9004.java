f62cbfc07aa47e47fb5b01f620703a2acd3ee6f4
黄玉东 <1410995914@qq.com>
2020-09-29 20:29:48 +0800

    Sentinel服务熔断功能中Ribbon系列提供者

---
+++ springcloud-01/cloudalibaba-provider-payment9004/src/main/java/com/hyd/springcloud/PaymentAlibabaMain9004.java
@@ -0,0 +1,18 @@
+package com.hyd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author 黄玉东
+ * @Date 2020/9/29 19:58
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentAlibabaMain9004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentAlibabaMain9004.class,args);
+    }
+}
---
+++ springcloud-01/cloudalibaba-provider-payment9004/src/main/java/com/hyd/springcloud/controller/PaymentController.java
@@ -0,0 +1,39 @@
+package com.hyd.springcloud.controller;
+
+import com.hyd.springcloud.bean.CommonResult;
+import com.hyd.springcloud.bean.Payment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+/**
+ * @Author 黄玉东
+ * @Date 2020/9/29 20:02
+ * @Version 1.0
+ */
+@RestController
+public class PaymentController
+{
+    @Value("${server.port}")
+    private String serverPort;
+
+    public static HashMap<Long, Payment> hashMap = new HashMap<>();
+    static{
+        hashMap.put(1L,new Payment(1L,"28a8c1e3bc2742d8848569891fb42181"));
+        hashMap.put(2L,new Payment(2L,"bba8c1e3bc2742d8848569891ac32182"));
+        hashMap.put(3L,new Payment(3L,"6ua8c1e3bc2742d8848569891xt92183"));
+    }
+
+    @GetMapping(value = "/paymentSQL/{id}")
+    public CommonResult<Payment> paymentSQL(@PathVariable("id") Long id){
+        Payment payment = hashMap.get(id);
+        CommonResult<Payment> result = new CommonResult(200,"from mysql,serverPort:  "+serverPort,payment);
+        return result;
+    }
+
+
+
+}
