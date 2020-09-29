bcf9dc9abec4b21bb8eb4a82c9a06afcdf9af8f7
黄玉东 <1410995914@qq.com>
2020-09-29 16:14:23 +0800

    SpringCloud Alibaba中Sentinel流控、降级、热点key限流

---
+++ springcloud-01/cloudalibaba-sentinel-service8401/src/main/java/com/hyd/springcloud/SentinelMain8401.java
@@ -0,0 +1,18 @@
+package com.hyd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author 黄玉东
+ * @Date 2020/9/28 16:06
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SentinelMain8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelMain8401.class,args);
+    }
+}
