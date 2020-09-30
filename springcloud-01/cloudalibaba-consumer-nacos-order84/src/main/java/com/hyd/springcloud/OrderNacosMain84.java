bee3ad4cfd2dd7fc6d4611f03e6afefd016a0a16
黄玉东 <1410995914@qq.com>
2020-09-30 10:06:11 +0800

    Sentinel服务熔断功能中Ribbon消费者fallback和blockHandler配置

---
+++ springcloud-01/cloudalibaba-consumer-nacos-order84/src/main/java/com/hyd/springcloud/OrderNacosMain84.java
@@ -0,0 +1,20 @@
+package com.hyd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Author 黄玉东
+ * @Date 2020/9/29 20:49
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class OrderNacosMain84 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain84.class,args);
+    }
+}
